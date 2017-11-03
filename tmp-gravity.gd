b0aacd64f11ab22323302afb100c723c646f8906
Cameron Reikes <cameronreikes@gmail.com>
2017-11-02 23:38:42 -0700

    Fixed the bounding box so it is now pixel perfect

--- tmp-gravity.gd
+++ tmp-gravity.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-export var speed = 0
+export var speed = 0.0
 var to_move = Vector2()
 
 func _ready():
